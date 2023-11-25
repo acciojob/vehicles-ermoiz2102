735f16d08dd534f12450043a556f955276fa2759
Moiz Khan <er.moiz2102@gmail.com>
2023-11-25 19:47:52 +0530

    Assignmen

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -10,6 +10,10 @@ public class Vehicle {
         return currentSpeed;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public int getCurrentDirection() {
         return currentDirection;
     }
