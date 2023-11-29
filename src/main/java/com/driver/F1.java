1913281561249ec6f174cfd38ea1dd44f4d0bdf7
Moiz Khan <er.moiz2102@gmail.com>
2023-11-29 22:58:16 +0530

    finally

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -19,29 +19,26 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
         newSpeed=getCurrentSpeed()+rate;
+        System.out.println(newSpeed);
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
             changeGear(1);
         }
         //for all other cases, change the gear accordingly
-        else if(newSpeed>=250)
-            changeGear(6);
-        else if (newSpeed>=201||newSpeed<=250) {
-            changeGear(5);
-        }
-        else if (newSpeed>=151||newSpeed<=200) {
-            changeGear(4);
-        }
-
-        else if (newSpeed>=101||newSpeed<=150) {
+        else if(newSpeed>0&&newSpeed<=50)
+            changeGear(1);
+        else if(newSpeed>50&&newSpeed<=100)
+            changeGear(2);
+        else if (newSpeed>100&&newSpeed<=150) {
             changeGear(3);
         }
-        else if (newSpeed>=51||newSpeed<=100) {
-            changeGear(2);
-        }
+        else if(newSpeed>150&&newSpeed<=200)
+            changeGear(4);
+        else if(newSpeed>200&&newSpeed<=250)
+            changeGear(5);
         else
-            changeGear(1);
+            changeGear(6);
         if(newSpeed > 0) {
 
             changeSpeed(newSpeed, getCurrentDirection());
